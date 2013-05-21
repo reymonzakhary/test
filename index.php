c055e7e5e5cc9a3767f81690754754b894c156e0
Ray Villalobos <rayvillalobos@Booth.local>
2013-05-20 19:56:39 -0700

    Basic Template

--- index.php
+++ index.php
@@ -2,9 +2,15 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" />
-	<title>Roux Conference</title>
+	<title>Title</title>
+	<link rel="stylesheet" href="_/css/bootstrap.css">
+	<link rel="stylesheet" href="_/css/mystyles.css">
 </head>
 <body>
- <p>Welcome to the Roux Conference</p>
+	<h2>Headline</h2>
+	<p>Welcome to the Roux Conference</p>
+
+	<script src="_/js/bootstrap.js"></script>
+	<script src="_/js/myscript.js"></script>
 </body>
 </html>
