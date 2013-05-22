10d093dcdda2958c7320e76eecbfd044a71b5479
Ray Villalobos <rayvillalobos@Booth.local>
2013-05-22 11:05:04 -0700

    reset index file

--- index.php
+++ index.php
@@ -2,22 +2,9 @@
 <html lang="en">
 <head>
   <meta charset="utf-8" />
-	<title>Roux Conference 2013</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="_/css/bootstrap.css">
-	<link rel="stylesheet" href="_/css/mystyles.css">
+	<title>Roux Conference</title>
 </head>
-<body id="home">
-	<section class="container">
-		<?php include '_/components/php/header.php'; ?>
-		<div class="content row">
-			<section class="main span8">
-			</section><!-- main -->
-			<section class="sidebar span4">
-			</section><!-- sidebar -->
-		</div><!-- content -->
-	</section><!-- container -->
-	<script src="_/js/bootstrap.js"></script>
-	<script src="_/js/myscript.js"></script>
+<body>
+ <p>Welcome to the Roux Conference</p>
 </body>
 </html>
