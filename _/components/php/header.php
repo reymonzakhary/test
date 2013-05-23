270a1641d2219d874d63209523cb7ae07a4a3261
Ray Villalobos <ray@lynda.com>
2013-05-22 22:28:08 -0700

    02_03 added basic navbar to header.php

--- _/components/php/header.php
+++ _/components/php/header.php
@@ -4,6 +4,17 @@
 			<section id="branding">
 				<a href="index.php"><img src="images/misc/ralogo_monogram.png" alt="Logo for Roux Conference"></a>
 			</section><!-- branding -->
+
+			<section class="navbar">
+				<ul class="nav navbar-nav">
+					<li><a href="index.php">Home</a></li>
+					<li><a href="venuetravel.php">Venue/Travel</a></li>
+					<li><a href="schedule.php">Schedule</a></li>
+					<li><a href="artists.php">Artists</a></li>
+					<li><a href="register.php">Register</a></li>
+				</ul><!-- nav -->
+			</section><!-- navbar -->
+
 		</header><!-- header -->
 	</div><!-- column -->
 </div><!-- content -->
