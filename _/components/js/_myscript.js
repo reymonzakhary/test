d544fc4da9d90aa4fce0fd7edb388e6e0aeeedc5
Ray Villalobos <ray@lynda.com>
2013-05-23 01:11:27 -0700

    added jquery code to drop menus automatically

---
+++ _/components/js/_myscript.js
@@ -0,0 +1,11 @@
+$(function() {
+
+
+	//make menus drop automatically
+	$('ul.nav li.dropdown').hover(function() {
+		$('.dropdown-menu', this).fadeIn();
+	}, function() {
+		$('.dropdown-menu', this).fadeOut('fast');
+	});//hover
+
+}); //jQuery is loaded
