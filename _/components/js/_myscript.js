5eda5a7bb4e717335f053adbd25c151fa19db35e
Ray Villalobos <ray@lynda.com>
2013-05-23 01:38:56 -0700

    auto activated nav menu items and copied header.php file to all pages

--- _/components/js/_myscript.js
+++ _/components/js/_myscript.js
@@ -1,5 +1,12 @@
 $(function() {
 
+	//highlight the current nav
+	$("#home a:contains('Home')").parent().addClass('active');
+	$("#schedule a:contains('Schedule')").parent().addClass('active');
+	$("#artists a:contains('Artists')").parent().addClass('active');
+	$("#venuetravel a:contains('Venue/Travel')").parent().addClass('active');
+	$("#register a:contains('Register')").parent().addClass('active');
+
 
 	//make menus drop automatically
 	$('ul.nav li.dropdown').hover(function() {
