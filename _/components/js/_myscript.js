fdffec61e7f19d25cf48a70785b8c903e5eb2a67
Ray Villalobos <ray@lynda.com>
2013-05-24 21:09:41 -0700

    added carousel

--- _/components/js/_myscript.js
+++ _/components/js/_myscript.js
@@ -15,7 +15,22 @@ $(function() {
 		$('.dropdown-menu', this).fadeOut('fast');
 	});//hover
 
-
+	//show tooltips
 	$("[data-toggle='tooltip']").tooltip({ animation: true});
+
+	//show modals
+
+	$('.modalphotos img').on('click', function() {
+		$('#modal').modal({
+			show: true,
+		})
+
+		var mysrc = this.src.substr(0, this.src.length-7) + '.jpg';
+		$('#modalimage').attr('src', mysrc);
+		$('#modalimage').on('click', function(){
+				$('#modal').modal('hide');
+		})//hide modal
+	});//show modal
+
 
 }); //jQuery is loaded
